1b13773083e099a855f94d6d2cd7ec65ebb9534b
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-10-30 10:51:59 +0200

    add update serializer and view

--- plant_watered_api/plant/serializers.py
+++ plant_watered_api/plant/serializers.py
@@ -14,3 +14,10 @@ class PlantCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plant
         fields = ['name', 'species', 'water_frequency_days', 'last_watered_date']
+
+
+class PlantUpdateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Plant
+        fields = ['last_watered_date']
--- plant_watered_api/plant/views.py
+++ plant_watered_api/plant/views.py
@@ -1,7 +1,14 @@
-from rest_framework import viewsets
+from datetime import datetime
+
+from rest_framework import viewsets,status
+from rest_framework.response import Response
 
 from plant.models import Plant
-from plant.serializers import PlantListSerializer, PlantCreateSerializer
+from plant.serializers import (
+    PlantUpdateSerializer, 
+    PlantListSerializer, 
+    PlantCreateSerializer
+)
 
 
 class PlantViewSet(viewsets.ModelViewSet):
@@ -10,4 +17,26 @@ class PlantViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == 'create':
             return PlantCreateSerializer
+        if self.action == 'update':
+            return PlantUpdateSerializer
         return PlantListSerializer
+    
+    def update(self, request, *args, **kwargs):
+        plant = self.get_object()
+        last_watered_date = request.data.get('last_watered_date')
+
+        if not last_watered_date:
+            return Response({"detail": "Last watered date is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            last_watered_date = datetime.strptime(last_watered_date, '%Y-%m-%d').date()
+        except ValueError:
+            return Response({"detail": "Invalid date format, expected YYYY-MM-DD."}, status=status.HTTP_400_BAD_REQUEST)
+
+        if last_watered_date > datetime.now().date():
+            return Response({"detail": "Last watered date cannot be in the future."}, status=status.HTTP_400_BAD_REQUEST)
+
+        plant.last_watered_date = last_watered_date
+        plant.save()
+
+        return Response({"detail": "Last watered date updated successfully."}, status=status.HTTP_200_OK)
