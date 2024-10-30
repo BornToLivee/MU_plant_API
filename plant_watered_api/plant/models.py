ba2e39b4146a6da8f568ac12db0dff6d288c49eb
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-10-30 10:07:58 +0200

    create plant model and add aps in settengs.py

--- plant_watered_api/plant/models.py
+++ plant_watered_api/plant/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Plant(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    species = models.CharField(max_length=255)
+    water_frequency_days = models.IntegerField()
+    last_watered_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
